7e9e2cb6fd22ffa8c3a4ca454eed0ce9128a971e
t-tullis <t.tullis34@gmail.com>
2018-03-12 12:09:08 -0700

    removed modules

--- server.js
+++ server.js
@@ -10,11 +10,23 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 
 //Routes
+
+//Root Route localhost:3000
 app.get('/', function(req,res){
   res.sendFile(__dirname + '/views/index.html');
+});
+
+app.get('/api', function(req,res){
+  base_url = 'https://blooming-retreat-98443.herokuapp.com/'
+  
+  res.json({'base_url':base_url})
 })
 
+
+//Server Setup
 app.listen(process.env.PORT || 3000, function(){
   console.log('Server is running')
 })
+
+
 
