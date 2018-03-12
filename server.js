9b84c391cf3d87ee35b20aaa972691d7e88abb2e
t-tullis <t.tullis34@gmail.com>
2018-03-12 12:31:58 -0700

    Added Documented EndPoints

--- server.js
+++ server.js
@@ -16,17 +16,34 @@ app.get('/', function(req,res){
   res.sendFile(__dirname + '/views/index.html');
 });
 
+//Documented API EndPoints
 app.get('/api', function(req,res){
   base_url = 'https://blooming-retreat-98443.herokuapp.com/'
+  documentation_url = 'https://github.com/t-tullis/personalapi' 
   
-  res.json({'base_url':base_url})
-})
+  res.json({
+    'documentation_url': documentation_url,
+    'base_url': base_url,
+    'endpoints':[
+    {
+    'method': 'GET',
+    'path': '/api',
+    'description': 'Describes all available endpoints'
+  },
+  {
+    'method': 'GET',
+    'path': '/profile',
+    'description': 'Who I am and Where I am from'
+  }
+
+    ]
+  });
+});
 
 
 //Server Setup
 app.listen(process.env.PORT || 3000, function(){
   console.log('Server is running')
 })
-
 
 
